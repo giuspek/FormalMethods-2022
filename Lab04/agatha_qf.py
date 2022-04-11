9f8fdd62f2b4cafb5b62f0129026f7fe08052d00
giuspek <giuspe@DESKTOP-RA97VIK.localdomain>
2022-04-11 15:35:52 +0200

    Fixing agatha_qf.py - lab04

--- Lab04/agatha_qf.py
+++ Lab04/agatha_qf.py
@@ -31,32 +31,34 @@ solver.add_assertion(Not(Function(richer, [killer,victim])))
 
 
 # Hidden condition: no one is richer than him-/herself (non-reflexivity)
-solver.add_assertion(ForAll(i, Not(Function(richer, [i,i]))))
+solver.add_assertion(ForAll([i], Not(Function(richer, [i,i]))))
 
 # Hidden condition: if i is richer than j then j is not richer than i (non-simmetry)
-solver.add_assertion(ForAll([i,j], Iff((Function(richer, [i,j])),(Not(Function(richer, [j,i]))))))
+solver.add_assertion(ForAll([i,j], Implies((Function(richer, [i,j])),(Not(Function(richer, [j,i]))))))
 
 # Charles hates no one that Agatha hates. 
-solver.add_assertion(ForAll(i, Implies(Function(hates, [agatha,Int(i)]), Not(Function(hates, [charles,i])))))
+solver.add_assertion(ForAll([i], Implies(Function(hates, [agatha,i]), Not(Function(hates, [charles,i])))))
 
 # Agatha hates everybody except the butler.
-solver.add_assertion(ForAll(i, Implies(NotEquals(i, butler), Function(hates, [agatha,i]))))
+solver.add_assertion(ForAll([i], Implies(NotEquals(i, butler), Function(hates, [agatha,i]))))
 
 solver.add_assertion(Function(hates, [agatha,charles]))
 solver.add_assertion(Function(hates, [agatha,agatha]))
 solver.add_assertion(Not(Function(hates, [agatha,butler])))
 
 # The butler hates everyone not richer than Aunt Agatha. 
-solver.add_assertion(ForAll(i, Implies(Not(Function(richer, [Int(i),agatha])), Function(hates, [butler,Int(i)]))))
+solver.add_assertion(ForAll([i], Implies(Not(Function(richer, [i,agatha])), Function(hates, [butler,i]))))
 
 # The butler hates everyone whom Agatha hates. 
-solver.add_assertion(ForAll(i, Implies(Function(hates, [agatha,Int(i)]), Function(hates, [butler,Int(i)]) )))
+solver.add_assertion(ForAll([i], Implies(Function(hates, [agatha,i]), Function(hates, [butler,i]) )))
 
 # Noone hates everyone. 
-solver.add_assertion(ForAll(i, Plus([Ite(Function(hates, [Int(i),Int(j)]),Int(1),Int(0)) for j in range(n)]) <= Int(2)))
+solver.add_assertion(ForAll([i], Plus([Ite(Function(hates, [i,Int(j)]),Int(1),Int(0)) for j in range(n)]) <= Int(2)))
 
 # Who killed Agatha? 
 num_solutions = 0
+
+print(solver.assertions)
 
 while solver.solve():
     num_solutions += 1
